59df18faac23c4013cdc761e2779bc54000a3c9a
shivam789851 <manjuimm2@gmail.com>
2025-10-01 11:54:31 +0530

    Update ptero-panel.sh

--- ptero-panel.sh
+++ ptero-panel.sh
@@ -27,7 +27,7 @@ echo -e "${NC}"
 echo -e "${GRN}🔥 Please Subscribe \n${NC}"
 
 for i in {1..3}; do
-  echo -ne "${CYN}Subscribing To ZYnexCode"
+  echo -ne "${CYN}Subscribing To zyexcode"
   for dot in {1..3}; do
     echo -n "."
     sleep 0.3
@@ -131,4 +131,5 @@ echo -e "${GRN}X-> Creating Admin User...${NC}"
 docker-compose run --rm panel php artisan p:user:make
 
 echo -e "${YEL}✅ Panel is installed! Enjoy now${NC}"
+
 
